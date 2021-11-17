55cf2e3917bc2bd3ae884528aca99945828e330c
nikos efthias <nikos@mugsoft.io>
2021-11-17 03:54:06 +0300

    create table and from tokio_postgres feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod query;
+#[cfg(feature = "tokio_postgres")]
+pub use tokio_postgres::Row;
 pub trait Table
 {
 	fn table_name() -> String;
--- src/query.rs
+++ src/query.rs
@@ -11,3 +11,47 @@ macro_rules! params {
 		&[$(&$param),*]
 	};
 }
+
+#[macro_export]
+macro_rules! table {
+	($name:tt{$($rust_key:ident $rust_type:ty : $db_name:tt $db_type:expr ),+}) => {
+		#[derive(Debug)]
+		struct $name {
+		$(
+			$rust_key:$rust_type
+		),+
+		}
+		impl $name{
+			pub fn new($($rust_key:$rust_type),+) -> $name{
+				Self{
+					$($rust_key),+
+				}
+			}
+			pub fn to_create_table_str()->String{
+				let fields = [
+					$(concat!("\"",stringify!($db_name),"\""," ",$db_type)),+
+				];
+				format!("CREATE TABLE IF NOT EXISTS {} ({})",stringify!($name).to_lowercase(),fields.join(","))
+			}
+		}
+		impl $crate::Table for $name{
+			fn table_name()->String{
+				stringify!($name).to_lowercase()
+			}
+		}
+		#[cfg(feature="tokio_postgres")]
+		impl From<&$crate::Row> for $name{
+			fn from(r:&$crate::Row)->Self{
+				Self{
+					$($rust_key:r.get(stringify!($rust_key))),+
+				}
+			}
+		}
+		#[cfg(feature="tokio_postgres")]
+		impl From<$crate::Row> for $name{
+			fn from(r:$crate::Row)->Self{
+				Self::from(&r)
+			}
+		}
+	};
+}
