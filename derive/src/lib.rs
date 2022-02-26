03f9d8d999ed7ff604ad8393eafb0b0416e9d7c1
nikos efthias <nikos@mugsoft.io>
2022-02-26 12:36:28 +0300

    cleanup

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,57 +1,12 @@
 mod parsers;
 mod pg;
+mod table;
 mod utils;
-
-use parsers::{parse_fields, FieldInfo, ParsedRootMeta};
-use proc_macro2::TokenStream;
-use quote::quote;
-use syn::{parse2, DeriveInput};
-
 pub(crate) use utils::Result;
 #[proc_macro_derive(Table, attributes(table))]
-pub fn parse_table(t: proc_macro::TokenStream) -> proc_macro::TokenStream
+pub fn table(t: proc_macro::TokenStream) -> proc_macro::TokenStream
 {
-	match run(t.into()) {
+	match table::table(t.into()) {
 		Ok(s) | Err(s) => s.into(),
 	}
-}
-fn run(t: TokenStream) -> Result<TokenStream>
-{
-	let root: DeriveInput = parse2(t).unwrap();
-	let name = &root.ident;
-	let ParsedRootMeta {
-		fields,
-		table_name,
-		index,
-		..
-	} = parsers::parse_root(&root)?;
-	let fields = parse_fields(&fields.named)?;
-	let field_names = fields.iter().map(|v| v.db_name.clone());
-	let pg_impls = pg::init(name, &table_name, &fields, &index);
-	let getters = field_getters(&fields);
-	Ok(quote!(
-	  impl #name{
-		  pub const fn __table_name() -> &'static str {
-			  #table_name
-		  }
-	  pub fn __columns()->&'static[&'static str]{
-			&[#(#field_names),*]
-	  }
-	#getters
-	  }
-	   #pg_impls
-	))
-}
-
-fn field_getters(fields: &[FieldInfo]) -> TokenStream
-{
-	let f = fields
-		.iter()
-		.map::<TokenStream, _>(|FieldInfo { name, db_name, .. }| {
-			let name = name.to_string();
-			format!("pub const fn __field_{name}()->&'static str{{\"{db_name}\"}}",)
-				.parse()
-				.unwrap()
-		});
-	quote!(#(#f)*)
 }
---
+++ derive/src/table.rs
@@ -0,0 +1,47 @@
+use super::{
+	parsers::{self, parse_fields, FieldInfo, ParsedRootMeta}, pg
+};
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse2, DeriveInput};
+
+pub fn table(t: TokenStream) -> Result<TokenStream>
+{
+	let root: DeriveInput = parse2(t).unwrap();
+	let name = &root.ident;
+	let ParsedRootMeta {
+		fields,
+		table_name,
+		index,
+		..
+	} = parsers::parse_root(&root)?;
+	let fields = parse_fields(&fields.named)?;
+	let field_names = fields.iter().map(|v| v.db_name.clone());
+	let pg_impls = pg::init(name, &table_name, &fields, &index);
+	let getters = field_getters(&fields);
+	Ok(quote!(
+	  impl #name{
+		  pub const fn __table_name() -> &'static str {
+			  #table_name
+		  }
+	  pub fn __columns()->&'static[&'static str]{
+			&[#(#field_names),*]
+	  }
+	#getters
+	  }
+	   #pg_impls
+	))
+}
+
+fn field_getters(fields: &[FieldInfo]) -> TokenStream
+{
+	let f = fields
+		.iter()
+		.map::<TokenStream, _>(|FieldInfo { name, db_name, .. }| {
+			let name = name.to_string();
+			format!("pub const fn __field_{name}()->&'static str{{\"{db_name}\"}}",)
+				.parse()
+				.unwrap()
+		});
+	quote!(#(#f)*)
+}
